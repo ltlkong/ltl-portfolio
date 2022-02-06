7d6ecf7c02cbf89f1492cb6660b39457054ddde0
Kongi <tielinli@yahoo.com>
2022-02-06 10:57:34 -0600

    Random

--- src/styles/themePrimary/index.tsx
+++ src/styles/themePrimary/index.tsx
@@ -1,9 +1,12 @@
+import generateGradient from '../../utils/generateGradient';
+
 const themePrimary = {
     colorPrimary: 'rgb(213, 217, 224)',
     colorSecondary: 'rgb(74, 109, 136)',
     colorNight: 'rgb(1, 12, 30)',
     colorDark: 'rgb(0, 30, 56)',
-    backgroundHome: 'linear-gradient(to right, #00416a, #e4e5e6)',
+    //backgroundHome: 'linear-gradient(to right, #00416a, #e4e5e6)',
+    backgroundHome: generateGradient(),
     textColorPrimary: 'rgb(0, 30, 56)',
     textMagic: {
         backgroundImage: 'linear-gradient(to right, #abbaab, #ffffff)',

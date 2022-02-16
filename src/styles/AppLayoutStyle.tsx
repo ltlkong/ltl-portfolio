32e7bb46983d1616bde16990ed4e8edf98001a72
Kongi <tielinli@yahoo.com>
2022-02-15 19:17:24 -0600

    Add transition

--- src/styles/AppLayoutStyle.tsx
+++ src/styles/AppLayoutStyle.tsx
@@ -1,26 +1,27 @@
 import { Theme } from './themePrimary';
 
 const styles = (theme: Theme) => ({
-    Nav: {
-        composes: 'd-flex flex-column fw-bolder',
-        right: '10%',
-        top: '10%',
+  Nav: {
+    composes: 'd-flex flex-column fw-bolder',
+    right: '10%',
+    top: '10%',
 
-        '& a': {
-            display: 'list-item',
-            padding: '.1em',
-            fontSize: '.9em',
-            color: theme.textColorPrimary,
-            '&:hover': {
-                color: theme.colorSecondary,
-            },
-        },
-
-        '& .active': {
-            color: theme.colorSecondary,
-            listStyle: 'square',
-        },
+    '& a': {
+      display: 'list-item',
+      padding: '.1em',
+      fontSize: '.9em',
+      color: theme.textColorPrimary,
+      transition: 'color .3s',
+      '&:hover': {
+        color: theme.colorSecondary,
+      },
     },
+
+    '& .active': {
+      color: theme.colorSecondary,
+      listStyle: 'square',
+    },
+  },
 });
 
 export default styles;

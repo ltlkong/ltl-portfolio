68e46a9c5c733cd4ed2390281c246acbf55b9a4a
Tielin Li <tielinli@yahoo.com>
2024-03-18 19:47:08 -0500

    edit

--- src/routes/links.tsx
+++ src/routes/links.tsx
@@ -3,18 +3,7 @@ const links = [
     to: "/",
     name: "Home",
     active: true,
-  },
-  {
-    to: "/contact",
-    name: "Contact",
-    active: true,
-  },
-
-  {
-    to: "/blog",
-    name: "Blog",
-    active: false,
-  },
+  }
 ];
 
 export default links;
